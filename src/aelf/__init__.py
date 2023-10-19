cba7575200e46dc621850f59f9b05552c4a9c862
wangyue <yue.wang@aelf.io>
2023-10-19 16:54:24 +0800

    feat:upgrade 1.5.0

--- src/aelf/__init__.py
+++ src/aelf/__init__.py
@@ -1,3 +1,4 @@
 from aelf.client import AElf
 from aelf.toolkits import AElfToolkit
-from aelf.types_pb2 import Transaction, Hash, Address, MinerList, StringInput, CandidateVote, PublicKeysList
+from aelf.types_pb2 import Transaction, Hash, Address, MinerList, StringInput, CandidateVote, PublicKeysList, \
+    TransferInput
